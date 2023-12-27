5100eb403090f4507fcd46d657f85db6aeca6624
ranggaggngntt <ranggaakb48@yahoo.com>
2023-12-27 11:27:22 -0800

    push

---
+++ index.php
@@ -0,0 +1,183 @@
+<html lang="en">
+   <head>
+      <title>achmadawdi.com</title>
+      <meta data-n-head="ssr" charset="utf-8">
+      <meta data-n-head="ssr" name="viewport" content="width=device-width, initial-scale=1">
+      <meta data-n-head="ssr" data-hid="description" name="description" content="">
+      <meta data-n-head="ssr" name="format-detection" content="telephone=no">
+      <link data-n-head="ssr" rel="icon" type="image/png" href="/logo.png">
+      <link data-n-head="ssr" data-hid="gf-prefetch" rel="dns-prefetch" href="https://fonts.gstatic.com/">
+      <link data-n-head="ssr" data-hid="gf-preconnect" rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+      <link data-n-head="ssr" data-hid="gf-preload" rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700">
+      <noscript data-n-head="ssr" data-hid="gf-noscript">
+         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700">
+      </noscript>
+      <link rel="stylesheet" href="tailwind.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+   </head>
+   <body>
+      <div id="__layout">
+         <div>
+            <nav class="drop-shadow-lg shadow-lg w-14 fixed h-screen py-10 flex flex-col justify-between transition-all bg-sidebar-black">
+               <div class="text-lg
+                  font-inter font-light
+                  grid grid-flow-col
+                  gap-x-4
+                  rotate-90
+                  transform-gpu
+                  list-link
+                  "><a href="#about" class="active">about</a> <a href="#works" class="">works</a> <a href="#contact" class="">contact</a></div>
+               <div class="text-lg grid grid-flow-row justify-center gap-y-3 social-media">
+                  <a href="https://github.com/ranggaggngntt">
+                     <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="svg-inline--fa fa-github">
+                        <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" class=""></path>
+                     </svg>
+                  </a>
+                  <a href="https://www.linkedin.com/in/rangga-saputra-46a861231/">
+                     <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="linkedin" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-linkedin">
+                        <path fill="currentColor" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" class=""></path>
+                     </svg>
+                  </a>
+                  <a href="https://www.instagram.com/ranggaads_/">
+                     <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="instagram" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svg-inline--fa fa-instagram">
+                        <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" class=""></path>
+                     </svg>
+                  </a>
+                  <a href="mailto:ranggaa.dwi@hotmail.com">
+                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-envelope">
+                        <path fill="currentColor" d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z" class=""></path>
+                     </svg>
+                  </a>
+               </div>
+            </nav>
+
+            <section id="about" class="w-full pl-14 py-10 lg:py-0">
+               <div class="container mx-auto container-lg px-10 lg:px-10">
+                  <div class="grid sm:grid-flow-row lg:grid-cols-2 place-items-center h-full lg:h-screen">
+                     <div class="text-black font-inter w-full grid grid-cols-1 gap-y-5 order-2 lg:order-1">
+                        <h1 class="text-5xl lg:text-6xl font-bold max-w-lg">
+                           Hello, I'm Rangga Dwi Saputra
+                        </h1>
+                        <p class="text-lg lg:text-xl font-normal">
+                           I am a <b>Python Dev</b> and <b>Mobile Dev</b> with a keen interest in application development. I am excited to leverage my skills and experience to develop innovative applications.
+                        </p>
+                        <div class="flex gap-x-4 text-lg lg:text-xl"><a href="#works"><button class="btn bg-black text-white">My works</button></a> <a href="https://www.linkedin.com/in/rangga-saputra-46a861231/" target="_blank" class="btn border-black border">More about me</a></div>
+                        <hr class="border-black">
+                        <div class="w-full h-11 border-black border rounded-md relative overflow-hidden">
+                           <div class="skills h-full w-max flex flex-wrap gap-x-2 items-center" id="skills">
+                              <span class="skillbar bg-black text-white">Learning About</span>
+                              <span class="skillbar bg-yellow-300"><i class="fa-brands fa-python"></i> Python</span>
+                              <span class="skillbar bg-green-300"><i class="fab fa-cplusplus"></i> C++</span>
+                              <span class="skillbar bg-blue-300"><i class="fab fa-flutter"></i> Flutter</span>
+                              <span class="skillbar bg-yellow-100"><i class="fab fa-kotlin"></i> Kotlin</span>
+                              <span class="skillbar bg-red-300"><i class="fab fa-php"></i> PHP</span>
+                              <span class="skillbar bg-yellow-200"><i class="fa-solid fa-terminal"></i> Bash</span>
+                            </div>
+                        </div>
+                     </div>
+                     <div class="w-full h-full lg:pl-40 flex justify-center items-center order-1 lg:order-2 pb-5 lg:pb-0">
+                     <div class="h-80 lg:h-3/4 rounded-lg" style="background: url(&quot;assets/me.jpg&quot;) center center / cover; width: 100%;"></div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+
+            <section id="works" class="bg-black w-full min-h-screen pl-14 py-20 text-white font-inter">
+               <div class="container mx-auto container-lg px-10 lg:px-10">
+                  <h1 class="font-bold text-5xl lg:text-6xl">Recent Work</h1>
+                  <div class="grid lg:grid-cols-2 py-10 gap-10 font-inter">
+                     <div class="grid group sm:grid-flow-row lg:grid-cols-2">
+                        <div class="pt-2 lg:pt-0 lg:pr-5 order-2 lg:order-1">
+                           <h2 class="pr-1 text-white tracking-wider text-2xl text-stroke font-bold inline-block" style="background:linear-gradient(to bottom,#ffffff00 0,#ffffff00 55%,#a52ee5 56%,#a52ee5 100%);">
+                              BGDF Anime
+                           </h2>
+                           <p class="py-4 text-gray-200 group-hover:text-white transition-all">
+                              A Web app that is for streaming Anime
+                           </p>
+                           
+                        </div>
+                        <div class="border h-full min-h-2 border-white rounded-md bg-cover bg-center order-1 lg:order-2" style="background-image:url(assets/animeweb.png);background-size: contain;"></div>
+                     </div>
+                     
+                     <div class="grid group sm:grid-flow-row lg:grid-cols-2">
+                        <div class="pt-2 lg:pt-0 lg:pr-5 order-2 lg:order-1">
+                           <h2 class="pr-1 text-white tracking-wider text-2xl text-stroke font-bold inline-block" style="background:linear-gradient(to bottom,#ffffff00 0,#ffffff00 55%,#0069b2 56%,#0069b2 100%);">
+                              Dinasty Coin
+                           </h2>
+                           <p class="py-4 text-gray-200 group-hover:text-white transition-all">
+                              Profile of Coin Crypto name Dinasty Coin
+                           </p>
+                        </div>
+                        <div class="border h-full min-h-2 border-white rounded-md bg-cover bg-center order-1 lg:order-2" style="background-image:url(assets/dinastycoin.png);background-size: contain;"></div>
+                     </div>
+                     <div class="grid group sm:grid-flow-row lg:grid-cols-2">
+                        <div class="pt-2 lg:pt-0 lg:pr-5 order-2 lg:order-1">
+                           <h2 class="pr-1 text-white tracking-wider text-2xl text-stroke font-bold inline-block" style="background:linear-gradient(to bottom,#ffffff00 0,#ffffff00 55%,#4D8ACD 56%,#4D8ACD 100%);">
+                              TWG Bot
+                           </h2>
+                           <p class="py-4 text-gray-200 group-hover:text-white transition-all">
+                              Automation bot trading For Binomo
+                           </p>
+                           
+                        </div>
+                        <div class="border h-full min-h-2 border-white rounded-md bg-cover bg-center order-1 lg:order-2" style="background-image:url(assets/twg.png);background-size: contain;"></div>
+                     </div>
+                     <div class="grid group sm:grid-flow-row lg:grid-cols-2">
+                        <div class="pt-2 lg:pt-0 lg:pr-5 order-2 lg:order-1">
+                           <h2 class="pr-1 text-white tracking-wider text-2xl text-stroke font-bold inline-block" style="background:linear-gradient(to bottom,#ffffff00 0,#ffffff00 55%,#FFB013 56%,#FFB013 100%);">
+                              AnimeVerz
+                           </h2>
+                           <p class="py-4 text-gray-200 group-hover:text-white transition-all">
+                              A Mobile app that is for streaming Anime
+                           </p>
+                           <a href="https://github.com/ranggaggngntt/anime-streaming" target="_blank" class="btn border border-white inline-block">View Project</a>
+                        </div>
+                        <div class="border h-full min-h-2 border-white rounded-md bg-cover bg-center order-1 lg:order-2" style="background-image:url(assets/animeapp.png);background-size: contain;"></div>
+                     </div>
+                     <div class="grid group sm:grid-flow-row lg:grid-cols-2">
+                        <div class="pt-2 lg:pt-0 lg:pr-5 order-2 lg:order-1">
+                           <h2 class="pr-1 text-white tracking-wider text-2xl text-stroke font-bold inline-block" style="background:linear-gradient(to bottom,#ffffff00 0,#ffffff00 55%,#473BD6 56%,#473BD6 100%);">
+                              Compress-Convert
+                           </h2>
+                           <p class="py-4 text-gray-200 group-hover:text-white transition-all">
+                              Tools that provide to compress video/image or convert some document
+                           </p>
+                           <a href="https://github.com/ranggaggngntt/Compress-Convert" target="_blank" class="btn border border-white inline-block">View Project</a>
+                        </div>
+                        <div class="border h-full min-h-2 border-white rounded-md bg-cover bg-center order-1 lg:order-2" style="background-image:url(assets/compress.png);background-size: contain;"></div>
+                     </div>
+                     <div class="grid group sm:grid-flow-row lg:grid-cols-2">
+                        <div class="pt-2 lg:pt-0 lg:pr-5 order-2 lg:order-1">
+                           <h2 class="pr-1 text-white tracking-wider text-2xl text-stroke font-bold inline-block" style="background:linear-gradient(to bottom,#ffffff00 0,#ffffff00 55%,#ffffff 56%,#ffffff 100%);">
+                              Covid App
+                           </h2>
+                           <p class="py-4 text-gray-200 group-hover:text-white transition-all">
+                              Mobile App that for monitoring Covid
+                           </p>
+                           <a href="https://github.com/ranggaggngntt/Covid-19" target="_blank" class="btn border border-white inline-block">View Project</a>
+                        </div>
+                        <div class="border h-full min-h-2 border-white rounded-md bg-cover bg-center order-1 lg:order-2" style="background-image:url(assets/covid.png);background-size: contain;"></div>
+                     </div>
+                  </div>
+               </div>
+            </section>
+
+            <!-- <section id="poc" class="bg-yellow-100 w-full pl-14 py-20 text-white font-inter">
+               <div class="container mx-auto container-lg px-10 lg:px-10">
+                  <h1 class="font-bold text-5xl lg:text-6xl text-black">How to use</h1>
+                  <div class="grid sm:grid-flow-row lg:grid-cols-2 place-items-center h-full lg:h-screen">
+                        <h1 class="text-5xl lg:text-1xl font-bold max-w-lg text-black">Test</h1>
+                        <iframe src="assets/poc.mp4" frameborder="0" width="700px" height="300px"></iframe>
+                  </div>
+               </div>
+            </section> -->
+
+            <?php include('template/footer.html'); ?>
+
+         </div>
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.4/gsap.min.js"></script>
+      <script src="script.js"></script>
+   </body>
+</html>
